36980a6bd3330ec83d2094baed73cef2626fb314
rohit saroha <rohitsaroha@rohits-MacBook-Air.local>
2020-03-30 17:17:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './components/homepage/homepage.component'
+
+function App() {
+  return (
+    <div>
+      <Homepage>
+      </Homepage>
+    </div>
+  );
+}
+
+export default App;

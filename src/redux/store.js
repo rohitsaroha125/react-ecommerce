a9b5fab29c032a960b6513b880f9b8b7939bc5d5
rohit saroha <rohitsaroha@rohits-MacBook-Air.local>
2020-04-28 18:42:52 +0530

    Hide logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,12 @@ import rootReducer from './root.reducer.js'
 
 import { persistStore } from 'redux-persist'
 
-const middleware=[logger]
+const middleware=[]
+
+if(process.env.NODE_ENV==="development")
+{
+    middleware.push(logger)
+}
 
 export const store=createStore(rootReducer,applyMiddleware(...middleware ))
 
